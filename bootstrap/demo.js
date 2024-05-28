0e414434c2c53386cfc96583b4828c4c2c707a57
gvswetha <113625900+gvswetha@users.noreply.github.com>
2024-05-28 14:36:35 +0530

    Add files via upload

---
+++ bootstrap/demo.js
@@ -0,0 +1,11 @@
+function demo(){
+
+var actor ="thalaphaty" 
+var player="shreyas iyer"
+var movie ="ghilli"
+console.log("favorite actor:"+ actor)
+console.log("favorite player:" +player)
+console.log("favorite movie:" +movie)
+
+}
+demo()
